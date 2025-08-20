e136e6d93fff1a769be126b8780bd4941ef90d6e
nazilealic <nazilealic@gmail.com>
2025-08-20 14:01:52 +0300

    feat: student controller added

--- src/main/java/com/example/demo4/controller/StudentController.java
+++ src/main/java/com/example/demo4/controller/StudentController.java
@@ -1,11 +1,44 @@
 package com.example.demo4.controller;
 
+import com.example.demo4.dto.StudentRequestDTO;
+import com.example.demo4.dto.StudentResponseDTO;
+import com.example.demo4.service.StudentService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    // CREATE
+    @PostMapping
+    public StudentResponseDTO createStudent(@RequestBody StudentRequestDTO dto) {
+        return studentService.createStudent(dto);
+    }
+
+    // READ
+    @GetMapping
+    public List<StudentResponseDTO> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    // UPDATE
+    @PutMapping("/{id}")
+    public StudentResponseDTO updateStudent(@PathVariable Long id, @RequestBody StudentRequestDTO dto) {
+        return studentService.updateStudent(id, dto);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudent(id);
+    }
 
     }
 
